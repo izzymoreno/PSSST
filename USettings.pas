2494966aee1071b555abcb0810c68e1f23d28d5d
Alexandr Vladimirovich Bazhenov <izzy84@mail.ru>
2022-10-19 12:29:43 +0300

    Add files via upload

--- USettings.pas
+++ USettings.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, Spin, ComCtrls, ExtCtrls, UTableHitScore;
+  Dialogs, StdCtrls, Spin, ComCtrls, ExtCtrls, UTableHitScore, jpeg;
 
 const WM_CLOSE_MODAL = WM_USER + 1;
 
@@ -25,14 +25,24 @@ type
     Label5: TLabel;
     Button3: TButton;
     Label6: TLabel;
+    EditRegisterKey: TEdit;
+    RegisterIt: TButton;
+    Label7: TLabel;
+    CheckBox1: TCheckBox;
+    TrackBarLevels: TTrackBar;
+    Label8: TLabel;
+    EditLevels: TEdit;
+    Label9: TLabel;
+    Image1: TImage;
     procedure FormCreate(Sender: TObject);
     PROCEDURE WndProc(var Msg: TMessage); override;
-    procedure FormShow(Sender: TObject);
     procedure Button1Click(Sender: TObject);
     procedure TrackBarWormsChange(Sender: TObject);
     procedure Button2Click(Sender: TObject);
     procedure TrackBarFlyesChange(Sender: TObject);
     procedure Button3Click(Sender: TObject);
+    procedure RegisterItClick(Sender: TObject);
+    procedure TrackBarLevelsChange(Sender: TObject);
 
   private
     { Private declarations }
@@ -46,7 +56,7 @@ type
 implementation
 
 uses
-Unit1;
+Unit1, UOwl;
 {$R *.dfm}
 
 PROCEDURE TSettingsForm.WndProc(var Msg: TMessage);
@@ -66,7 +76,7 @@ begin
   postmessage(self.Handle, WM_CLOSE_MODAL, 0, 0);
   SettingsForm.ModalResult := mrOk;
   Form1.DestroyLevel;
-  Form1.InitLevel;
+  Form1.InitLevel();
 end;
 
 procedure TSettingsForm.Button2Click(Sender: TObject);
@@ -89,16 +99,53 @@ begin
 FormTableScore := TFormTableScore.Create(Application);
 self.TrackBarWorms.Min := 1;
 self.TrackBarFlyes.Min := 1;
+self.TrackBarLevels.Min := 1;
+self.RadioGroup1.Enabled := False;
+self.EditPlayerName.Enabled := False;
+self.EditWorms.Enabled := False;
+self.EditFlyes.Enabled := False;
+self.EditLevels.Enabled := False;
+self.TrackBarLevels.Enabled := False;
+self.TrackBarWorms.Enabled := False;
+self.TrackBarFlyes.Enabled := False;
+self.Button1.Enabled := False;
+self.Button2.Enabled := False;
+self.Button3.Enabled := False;
+self.CheckBox1.Enabled := False;
+
 end;
 
-procedure TSettingsForm.FormShow(Sender: TObject);
+procedure TSettingsForm.RegisterItClick(Sender: TObject);
 begin
-//btnConnect.Click;
+if (self.EditRegisterKey.Text = 'xUSSR') or (self.EditRegisterKey.Text = 'ÑÑÑÐ') then
+   begin
+   self.RadioGroup1.Enabled := True;
+   self.EditPlayerName.Enabled := True;
+   self.EditWorms.Enabled := True;
+   self.EditFlyes.Enabled := True;
+   self.TrackBarWorms.Enabled := True;
+   self.TrackBarFlyes.Enabled := True;
+   self.EditLevels.Enabled := True;
+   self.TrackBarLevels.Enabled := True;
+   self.Button1.Enabled := True;
+   self.Button2.Enabled := True;
+   self.Button3.Enabled := True;
+   self.CheckBox1.Enabled := True;
+
+   end;
 end;
 
 procedure TSettingsForm.TrackBarFlyesChange(Sender: TObject);
 begin
 EditFlyes.Text := inttostr(self.TrackBarFlyes.Position);
+end;
+
+procedure TSettingsForm.TrackBarLevelsChange(Sender: TObject);
+begin
+EditLevels.Text := inttostr(self.TrackBarLevels.Position);
+{if self.TrackBarLevels = 1 then ImageMenu1.Picture.LoadFromFile('RWorm1.bmp');
+if self.TrackBarLevels = 1 then ImageMenu1.Picture.LoadFromFile('RFly1.bmp');
+if self.TrackBarLevels = 1 then ImageMenu1.Picture.LoadFromFile('RWorm1&RFly1.bmp');}
 end;
 
 procedure TSettingsForm.TrackBarWormsChange(Sender: TObject);
